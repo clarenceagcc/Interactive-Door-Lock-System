fb4074ef7db8f37379127f9a9436877c3577d8fe
Clarence <agccclarence@gmail.com>
2025-03-01 18:32:38 +0800

    fix preprocessing

--- pages/face_recognition.py
+++ pages/face_recognition.py
@@ -24,10 +24,11 @@ def load_saved_faces():
 # Preprocess the image
 def preprocess_image(image):
     transform = transforms.Compose([
-        transforms.Resize((224, 224)),
+        transforms.Resize((224, 224)),  # Change based on your input size
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225])
+        transforms.Normalize(mean=[0.5, 0.5, 0.5], std=[0.5, 0.5, 0.5])
     ])
+
     image = transform(image).unsqueeze(0)  # Add batch dimension
     return image
 
@@ -52,7 +53,7 @@ def compare_faces(model, image1, image2, threshold=0.5):
     st.write(f"Similarity score: {similarity_score}")
     
     # Check if faces match based on a threshold
-    if similarity_score <= threshold:
+    if similarity_score < threshold:
         st.success("The faces match!")
     else:
         st.error("The faces do not match.")
